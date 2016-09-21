0f2f12625ae146f7e7de2cd1dbe8a5e6e64a8db0
Eduardo del Palacio Lirola <eddydpyl@gmail.com>
2016-09-21 16:56:33 +0200

    Github fucked up

---
+++ app/src/main/java/com/eduardo/fabs/Model/UserCategory.java
@@ -0,0 +1,7 @@
+package com.eduardo.fabs.model;
+
+/**
+ * Created by Eduardo on 02/08/2016.
+ */
+
+public enum UserCategory {COMPLETED, WATCHING, READING, ONHOLD, DROPPED, PLANTOWATCH, PLANTOREAD}
